4815437d136ebb5c90d75270c8fa3cc809483148
Rajiv Kulkarni <rajiv1628@gmail.com>
2023-02-27 21:19:09 +0530

    final commit

--- content_script.js
+++ content_script.js
@@ -1,3 +1,10 @@
+const scrollIntoView = (element) => {
+  const rect = element.getBoundingClientRect();
+  const top = rect.top + window.pageYOffset;
+  const middle = top - window.innerHeight / 2 + rect.height / 2;
+  window.scrollTo(0, middle);
+};
+
 if (
   window.location.href.indexOf(
     "https://www.linkedin.com/search/results/people"
@@ -74,42 +81,3 @@ if (
     }
   });
 }
-
-// const connectButtons = document.getElementsByClassName(
-//   "artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"
-// );
-// // Convert the HTMLCollection to an array and slice it to get the desired elements
-// const selectedElements = Array.prototype.slice.call(
-//   connectButtons,
-//   2,
-//   12
-// );
-// console.log(selectedElements.length);
-// const delay = Math.floor(Math.random() * 6000) + 1000; // random delay between 5-10 seconds
-// let count = 0;
-// //  Clicks the connect button, waits for 3 seconds, clicks the send now button, sends a
-// //  message to the background script, increments the count, and calls the function again
-// const clickButton = function (index) {
-//   selectedElements[index].click();
-//   setTimeout(() => {
-//     // Find and click the send now button
-//     const sendButton = document.querySelector(
-//       'button[aria-label="Send now"]'
-//     );
-//   // The above code is clicking the connect button on the linkedin profile.
-//     if (sendButton) {
-//       sendButton.click();
-//       console.log(index, "Connect Button Clicked 🚀");
-//       chrome.runtime.sendMessage({ message: "click_connect" });
-//       count++;
-//     } else {
-//       console.error("Send button not found");
-//     }
-//     if (index < selectedElements.length - 1) {
-//       setTimeout(() => clickButton(index + 1), delay);
-//     } else {
-//       console.log(count, "Connect Buttons Clicked 🚀");
-//     }
-//   }, 1000);
-// };
-// clickButton(0);
