eabcdb4ed6bf3a9f4f4d7460f46f0d7cd51ebb85
Rajiv Kulkarni <rajiv1628@gmail.com>
2023-02-27 11:52:05 +0530

    file updates, script working

--- content_script.js
+++ content_script.js
@@ -17,42 +17,99 @@ if (
       const connectButtons = document.getElementsByClassName(
         "artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"
       );
+
       // Convert the HTMLCollection to an array and slice it to get the desired elements
       const selectedElements = Array.prototype.slice.call(
         connectButtons,
         2,
         12
       );
+
       console.log(selectedElements.length);
+
       const delay = Math.floor(Math.random() * 6000) + 1000; // random delay between 5-10 seconds
       let count = 0;
+
       //  Clicks the connect button, waits for 3 seconds, clicks the send now button, sends a
       //  message to the background script, increments the count, and calls the function again
       const clickButton = function (index) {
-        selectedElements[index].click();
-        setTimeout(() => {
-          // Find and click the send now button
-          const sendButton = document.querySelector(
-            'button[aria-label="Send now"]'
-          );
-        // The above code is clicking the connect button on the linkedin profile.
-          if (sendButton) {
-            sendButton.click();
-            console.log(index, "Connect Button Clicked 🚀");
-            chrome.runtime.sendMessage({ message: "click_connect" });
+        const buttonText = selectedElements[index].innerText.trim();
+
+        if (buttonText === "Follow" || buttonText === "Connect") {
+          if (buttonText === "Connect") {
+            selectedElements[index].click();
+            setTimeout(() => {
+              // Find and click the send now button
+              const sendButton = document.querySelector(
+                'button[aria-label="Send now"]'
+              );
+
+              if (sendButton) {
+                sendButton.click();
+                console.log(index, "Connect Button Clicked 🚀");
+                chrome.runtime.sendMessage({ message: "click_connect" });
+                count++;
+              } else {
+                console.error("Send button not found");
+              }
+            }, 1000);
+          } else {
+            // buttonText === "Follow"
+            console.log(index, "Follow Button Clicked 🚀");
+            selectedElements[index].click();
             count++;
-          } else {
-            console.error("Send button not found");
           }
-          if (index < selectedElements.length - 1) {
-            setTimeout(() => clickButton(index + 1), delay);
-          } else {
-            console.log(count, "Connect Buttons Clicked 🚀");
-          }
-        }, 1000);
+        } else {
+          console.log(index, "Button Skipped ❌");
+        }
+
+        if (index < selectedElements.length - 1) {
+          setTimeout(() => clickButton(index + 1), delay);
+        } else {
+          console.log(count, "Buttons Clicked 🚀");
+        }
       };
+
       clickButton(0);
     }
   });
 }
 
+// const connectButtons = document.getElementsByClassName(
+//   "artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"
+// );
+// // Convert the HTMLCollection to an array and slice it to get the desired elements
+// const selectedElements = Array.prototype.slice.call(
+//   connectButtons,
+//   2,
+//   12
+// );
+// console.log(selectedElements.length);
+// const delay = Math.floor(Math.random() * 6000) + 1000; // random delay between 5-10 seconds
+// let count = 0;
+// //  Clicks the connect button, waits for 3 seconds, clicks the send now button, sends a
+// //  message to the background script, increments the count, and calls the function again
+// const clickButton = function (index) {
+//   selectedElements[index].click();
+//   setTimeout(() => {
+//     // Find and click the send now button
+//     const sendButton = document.querySelector(
+//       'button[aria-label="Send now"]'
+//     );
+//   // The above code is clicking the connect button on the linkedin profile.
+//     if (sendButton) {
+//       sendButton.click();
+//       console.log(index, "Connect Button Clicked 🚀");
+//       chrome.runtime.sendMessage({ message: "click_connect" });
+//       count++;
+//     } else {
+//       console.error("Send button not found");
+//     }
+//     if (index < selectedElements.length - 1) {
+//       setTimeout(() => clickButton(index + 1), delay);
+//     } else {
+//       console.log(count, "Connect Buttons Clicked 🚀");
+//     }
+//   }, 1000);
+// };
+// clickButton(0);
